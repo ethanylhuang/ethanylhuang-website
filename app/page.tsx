dfa3cf61db6996fa08f89ad2a15d8aff8e74d170
Ethan Huang <ethan.yl.huang@gmail.com>
2025-09-29 13:30:58 -0700

    clear

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,15 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Ethan Huang
       </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
+      <p className="mb-4">{`I'm Ethan, a high school student in the Bay Area into robotics. Hear are some of my projects`}</p>
       <div className="my-8">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
